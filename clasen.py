217ea29e3b1bb0a8bef39b0f1d2aba18572d79da
JulioLandaeta <107444227+JulioLandaeta@users.noreply.github.com>
2022-06-13 22:03:27 -0400

    Update clasen.py

--- clasen.py
+++ clasen.py
@@ -1,12 +1,9 @@
 # Clase_Py_funciones
 from tkinter import X, Y
 
-
 def sumar(a,b):
     c=a+b
     print(f'la suma de {a}+{b}={c}')
 X=int(input('igrese valor:'))
 Y=int(input('igrese valor:'))
 sumar(X,Y)
-
-print('pera con mango')
